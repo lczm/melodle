995ceaa07ab3b4024b27f63417d33a3723fa867d
jyorien <jyorin0206@gmail.com>
2025-01-18 17:02:27 +0800

    get ur player id

--- frontend/src/Room.tsx
+++ frontend/src/Room.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useWebSocket } from "./WebSocketContext";
 import { useParams } from "react-router-dom";
 
@@ -15,7 +15,7 @@ function Room() {
   const websocket = useWebSocket();
   const [gameState, setGameState] = useState<GameState>(GameState.LOBBY)
   const [playerId, setPlayerId] = useState()
- 
+
   useEffect(() => {
     if (!websocket) {
       console.error("WebSocket is not available.");
@@ -27,11 +27,13 @@ function Room() {
     websocket.onmessage = (e) => {
       const res = JSON.parse(e.data);
       console.log("Message from server:", res);
-      const state = "recording"
-      switch (state) {
+      switch (res.action) {
         case "turn":
+            console.log(`turn`)
+            console.log(res)
             setPlayerId(res.playerId)
             setGameState(GameState.WAITING)
+        
             break;
         case "recording":
             if (res.playerId != playerId) {
@@ -39,7 +41,11 @@ function Room() {
                 return
             }
             setGameState(GameState.RECORDING)
+            const audioBlob = res.audio
             break;
+        case "end":
+            setGameState(GameState.END)
+            break
       }
     };
   }, [websocket, roomCode]);
@@ -52,12 +58,30 @@ function Room() {
     websocket.send(JSON.stringify({ action: "start", roomId: roomCode }));
   };
 
+  const handleSubmitRecording = () => {
+    if (!websocket) {
+        console.error("WebSocket is not available.")
+        return
+    }
+    const req = {
+        action: "turn",
+        playerId: playerId,
+        audio: "blob"
+    }
+    websocket.send(JSON.stringify(req))
+  }
+
   return (
     <div>
         {gameState === GameState.LOBBY && <> <h1>Room Code: {roomCode}</h1>
         <button onClick={handleStart}>Start Game</button></>}
-        {gameState === GameState.WAITING && <h1>Please wait for your turn...</h1>}
-        {gameState === GameState.RECORDING && <h1>It's your turn</h1>}
+        {gameState === GameState.WAITING && <>
+            <h2>You are: Player {playerId}</h2>
+            <h1>Please wait for your turn...</h1>
+        </> }
+        {gameState === GameState.RECORDING && <><h1>ur turn</h1>
+            </>}
+        {gameState === GameState.END && <h1>What is the song?</h1>}
     
     </div>
   );
